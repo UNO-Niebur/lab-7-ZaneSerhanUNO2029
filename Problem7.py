30bfeebaf1c06d5bf2e9812781256036dfed2d38
ZaneSerhanUNO2029 <12106466@nebraska.edu>
2026-03-04 05:44:31 +0000

    Completed Lab7

--- Problem7.py
+++ Problem7.py
@@ -3,22 +3,19 @@
 #Assignment: Problem7.py
 #Project Euler Problem #7
 # Find10001stPrime
-#Approach:I just counted primes starting at 2 and checked each number with isPrime(). 
+#Approach:I just counted primes starting at 2 and checked each number in a large range with isPrime(). 
 # Every time I found a prime, I added to a counter until I got to the 10001st prime, then printed it.
 
 from NumberTests import isPrime
 
 def main():
-    prime_count = 0
-    num = 2
-
-    while True:
+    prime_count = 1 
+    for num in range(3, 2000000, 2): 
         if isPrime(num):
             prime_count += 1
             if prime_count == 10001:
                 print(f"The 10001st prime number is {num}")
                 break
-        num += 1
 
 if __name__ == '__main__':
     main()
